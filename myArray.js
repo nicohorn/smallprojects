b5be9e1e3b65793ab86dba525bb3d61a33c377a7
nicohorn <nicoskate000@gmail.com>
2020-09-04 17:55:08 -0300

    Reverse string program

--- myArray.js
+++ myArray.js
@@ -17,9 +17,33 @@ class myArray {
     this.length++;
     return this.length;
   }
+
+  pop(){
+    //Function that grabs the last item and takes it away from the array (deletes it);
+    const lastItem = this.data[this.length-1];
+    delete this.data[this.length-1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index){
+    //Function that deletes the item/object at a given index.
+    const item = this.data[index];
+    this.shiftItems(index);
+  }
+
+  shiftItems(index){
+    //Method that shifts the data of the array, given an index of an item to remove.
+    for(let i = index; i < this.length -1; i++){
+      this.data[i] = this.data[i+1];
+    }
+    //This last part is to delete the last item and decrement the length.
+    delete this.data[this.length-1];
+    this.length--;
+  }
 }
 
 const newArray = new myArray();
 newArray.push('Hello World!');
-newArray.push('Bye!');
+newArray.push('Bye');
 console.log(newArray);
