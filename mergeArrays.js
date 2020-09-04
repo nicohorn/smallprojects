9f6b8baccda37333acd5ec88231e0a1fb2787318
nicohorn <nicoskate000@gmail.com>
2020-09-04 18:13:57 -0300

    Merge arrays program

--- mergeArrays.js
+++ mergeArrays.js
@@ -2,19 +2,21 @@
 //Author: Nicolás Horn.
 
 function mergeArrays(arr1, arr2){
+  //Check if array 1 exists.
   if(arr1.length === 0){
     return arr2;
   }
+  //Check if array 2 exists.
   if(arr2.length === 0){
     return arr1;
   }
-
+  //Create an empty array.
   const mergedArray = [];
   let item1 = arr1[0];
   let item2 = arr2[0];
   let i = 1;
   let j = 1;
-
+  //While there is an element on each of the two arrays, execute.
   while(item1 || item2){
     if(item2 === undefined || item1 < item2){
       mergedArray.push(item1);
